090fe69dd562695d45917dde79fba49ef90da5c7
Qadeer Kulachi <165115669+Qadeerkhan786@users.noreply.github.com>
2024-10-29 21:52:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WmYmRQYb#kqZDm7B_qahC7ysLbxgBlxthZ6rwQx18rPKtn5hvGF4",
+MONGODB: process.env.MONGODB || "qadeer-shard-00-02.jugjz.mongodb.net:27017",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
